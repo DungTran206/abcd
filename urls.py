9a2b78c058be65fc96972e295784032953f34e99
DungTran206 <dungtrantmd1910@gmail.com>
2026-06-15 01:05:33 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,12 +4,7 @@ from . import views
 app_name = 'onlinecourse'
 
 urlpatterns = [
-    # ... các đường dẫn khác (nếu có) ...
-    
-    # Nộp bài thi
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    
-    # Xem kết quả thi
     path('course/<int:course_id>/submission/<int:submission_id>/result/', 
          views.show_exam_result, name='exam_result'),
 ]
